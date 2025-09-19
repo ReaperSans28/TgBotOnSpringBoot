9c2a80a1a128802e57dde94fef8351ba55bc4eee
ReaperSans28 <smileuwu321@gmail.com>
2025-09-19 19:38:46 +0300

    init

---
+++ src/test/java/com/zoo/TgBotOnSpringBoot/TgBotOnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.TgBotOnSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotOnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
